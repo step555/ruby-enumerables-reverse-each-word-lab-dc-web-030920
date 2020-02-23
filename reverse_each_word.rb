c3944e97c962c291d4178b9e1d0f1131b6b9c08c
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-23 15:06:52 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,11 @@ def reverse_each_word(string)
   return_array.join(" ")
 end
 
+# def reverse_each_word(string)
+#   original_array = string.split
+#   return_array = []
+#   original_array.each do |string|
+#     return_array << string.reverse
+#   end
+#   return_array.join(" ")
+# end
