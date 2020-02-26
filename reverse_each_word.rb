83c38b3043a77aaa1993ba86158ce7dd06ceda71
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-26 14:43:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
-# def reverse_each_word(string)
-#   original_array = string.split
-#   return_array = []
-#   return_array = original_array.collect{|string| string.reverse}
-#   return_array.join(" ")
-# end
+def reverse_each_word(string)
+  original_array = string.split
+  return_array = []
+  return_array = original_array.collect{|string| string.reverse}
+  return_array.join(" ")
+end
 
 # def reverse_each_word(string)
 #   original_array = string.split
